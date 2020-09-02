e525e727c41074109ac60323bda02ffab7c68a18
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-09-02 13:20:58 +0300

    setup sample packets

---
+++ src/js/character.js
@@ -0,0 +1,12 @@
+/**
+ * Функция конструктор Character;
+ * @param {string} name имя пользователя.
+ * @param {string} type тип оружия.
+ * @throws {InvalidArgumentException}
+ */
+
+// eslint-disable-next-line no-unused-vars
+function Character(name, type) {
+  this.name = name;
+  this.type = type;
+}
